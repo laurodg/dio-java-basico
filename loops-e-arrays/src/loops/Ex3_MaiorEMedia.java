7a27ae2e9b15af428f384466c619cd027c6c8d5d
laurodg <laurodg@gmail.com>
2023-10-05 10:59:11 -0300

    Atual

---
+++ loops-e-arrays/src/loops/Ex3_MaiorEMedia.java
@@ -0,0 +1,15 @@
+package loops;
+
+import org.jcp.xml.dsig.internal.SignerOutputStream;
+
+public class Ex3_MaiorEMedia {
+    public static void main(String[] args) {
+        scanner scan = new Scanner (System.ini);
+
+        int num;
+
+        System.out.println("Número: ");
+        numero = scan.nextInt();
+        
+    }
+}
